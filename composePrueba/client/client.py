0fca708af2bb0e31f4ef23ca9ebfda3c9fdf0244
Conchi Carcedo Carnero <conchicarcedo@correo.ugr.es>
2018-01-18 17:29:28 +0100

    Ejercicio 9 correcciones

--- composePrueba/client/client.py
+++ composePrueba/client/client.py
@@ -1,10 +1,9 @@
-import httplib2
+import requests
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == '__main__':
-   httplib2.debuglevel     = 0
-   h = httplib2.Http()
-   http = h.HTTPConnection("http://127.0.0.1/vias", 5000)
-
-   response, content = http.request("GET")
-   print (http.getresponse().read())
-  
+@app.route('/vias')
+def get_data():
+    while True:
+        requests.get('http://127.0.0.1:5000').content
+        print('Peticion')
